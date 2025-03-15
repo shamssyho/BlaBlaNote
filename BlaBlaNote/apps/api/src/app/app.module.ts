f1c6c9690f2be938ac8b4ad3d6d6b003bd6310db
shamssyho <shamss123012301@gmail.com>
2025-03-15 02:21:16 +0100

    add prisma service

--- BlaBlaNote/apps/api/src/app/app.module.ts
+++ BlaBlaNote/apps/api/src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { UserController } from './user/user.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [PrismaModule],
+  controllers: [AppController, UserController],
   providers: [AppService],
 })
 export class AppModule {}
