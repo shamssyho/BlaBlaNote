e7ca269b639e6b42f808dc2623ce723928033858
shamssyho <shamss123012301@gmail.com>
2025-03-16 16:52:58 +0100

    add crud user

---
+++ BlaBlaNote/apps/api/src/app/user/dto/create-user.dto.ts
@@ -0,0 +1,16 @@
+import { IsString, IsEmail, MinLength } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  firstName: string;
+
+  @IsString()
+  lastName: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @MinLength(6)
+  password: string;
+}
