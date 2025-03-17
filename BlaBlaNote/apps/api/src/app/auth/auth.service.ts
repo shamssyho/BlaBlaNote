633e1e52dc2b47824e96abf08274b1479a2ec738
shamssyho <shamss123012301@gmail.com>
2025-03-17 04:28:33 +0100

    improve file

--- BlaBlaNote/apps/api/src/app/auth/auth.service.ts
+++ BlaBlaNote/apps/api/src/app/auth/auth.service.ts
@@ -1,29 +1,25 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
-import { UserService } from '../user/user.service';
-import * as bcrypt from 'bcrypt';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class AuthService {
   constructor(
-    private readonly userService: UserService,
+    private readonly prisma: PrismaService,
     private readonly jwtService: JwtService
   ) {}
 
   async validateUser(email: string, password: string) {
-    const user = await this.userService.getUserByEmail(email);
-    if (!user) throw new UnauthorizedException('Invalid credentials');
+    const user = await this.prisma.user.findUnique({ where: { email } });
 
-    const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) throw new UnauthorizedException('Invalid credentials');
-
-    return user;
+    if (user && user.password === password) {
+      return user;
+    }
+    return null;
   }
 
   async login(user: any) {
-    const payload = { sub: user.id, email: user.email };
-    return {
-      access_token: this.jwtService.sign(payload),
-    };
+    const payload = { sub: user.id, email: user.email, role: user.role }; // ✅ Ajoute le rôle
+    return { access_token: this.jwtService.sign(payload) };
   }
 }
