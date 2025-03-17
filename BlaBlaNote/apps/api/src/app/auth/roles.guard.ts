c7f23312eed2faa0862ade404de12503db77d32a
shamssyho <shamss123012301@gmail.com>
2025-03-17 04:28:43 +0100

    update

--- BlaBlaNote/apps/api/src/app/auth/roles.guard.ts
+++ BlaBlaNote/apps/api/src/app/auth/roles.guard.ts
@@ -9,13 +9,16 @@ import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class RolesGuard implements CanActivate {
-  constructor(private reflector: Reflector, private jwtService: JwtService) {}
+  constructor(
+    private readonly reflector: Reflector,
+    private readonly jwtService: JwtService
+  ) {}
 
   canActivate(context: ExecutionContext): boolean {
-    const requiredRoles = this.reflector.getAllAndOverride<string[]>('roles', [
-      context.getHandler(),
-      context.getClass(),
-    ]);
+    const requiredRoles = this.reflector.get<string[]>(
+      'roles',
+      context.getHandler()
+    );
 
     if (!requiredRoles) return true;
 
@@ -27,10 +30,11 @@ export class RolesGuard implements CanActivate {
     const token = authHeader.split(' ')[1];
     const decoded = this.jwtService.verify(token);
 
-    if (!requiredRoles.includes(decoded.role)) {
+    if (!decoded || !decoded.role || !requiredRoles.includes(decoded.role)) {
       throw new ForbiddenException('You do not have permission');
     }
 
+    request.user = decoded;
     return true;
   }
 }
