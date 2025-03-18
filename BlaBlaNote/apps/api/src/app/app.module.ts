02b858ca85b19f91d9dccc5a3927c100862dfc7a
shamssyho <shamss123012301@gmail.com>
2025-03-18 02:42:52 +0100

    add note crud

--- BlaBlaNote/apps/api/src/app/app.module.ts
+++ BlaBlaNote/apps/api/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { NoteModule } from './note/note.module';
 
 @Module({
-  imports: [PrismaModule, UserModule, AuthModule],
+  imports: [PrismaModule, UserModule, AuthModule, NoteModule],
   controllers: [AppController],
   providers: [AppService],
 })
